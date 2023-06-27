984d6bd5575ca7dfc0183e1391392f4207fbf791
AlexTenghc <74139600+AlexTenghc@users.noreply.github.com>
2023-06-27 17:17:05 -0500

    Create 300.py

---
+++ 300.py
@@ -0,0 +1,15 @@
+"""Given an integer array nums, return the length of the longest strictly increasing 
+subsequence
+."""
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis = [1] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    lis[i] = max(lis[i], lis[j] + 1)
+
+
+        return max(lis)
+
+                

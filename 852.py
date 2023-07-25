3e4d44c9d290fd20d99faae351c7700f9dcf9685
AlexTenghc <74139600+AlexTenghc@users.noreply.github.com>
2023-07-24 23:05:00 -0400

    Update 852.py

--- 852.py
+++ 852.py
@@ -20,3 +20,23 @@ class Solution:
                 r = mid
 
         return l
+
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr)
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if arr[mid] > arr[mid+1] and arr[mid] > arr[mid-1]:
+                return mid
+
+            elif arr[mid+1] > arr[mid]:
+                l = mid + 1
+
+            else:
+                r = mid
+
+        
+
